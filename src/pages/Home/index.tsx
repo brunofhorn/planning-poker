ab3424d1336ef7afec5fea95ab049a134ec03baa
corpp02535 <brunohorn@ext.cvccorp.com.br>
2022-06-29 10:50:30 -0300

    Configuração da Página Inicial

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,24 @@
-import {Grid} from '@material-ui/core';
+import { Grid, Slide } from '@material-ui/core';
+import { useMatch } from 'react-router-dom';
 
-export const Home = ()=>{
+export const Home = () => {
+    const isJoin = useMatch('/join');
+
     return (
         <>
-            <Grid>
-                <div>teste router</div>
+            <Grid container direction='column' justify='center' alignItems='center' spacing={2}>
+                <Grid container item sm={12} lg={11} justify='center' alignItems='center' spacing={3}>
+                    <Grid item sm={12} lg={6}>
+                        <Slide in={true} direction='up' timeout={1500}>
+                            <div className='HomePageContainer'>
+                                Jogos RECENTES
+                            </div>
+                        </Slide>
+                    </Grid>
+                    <Grid item sm={12} lg={6}>
+                        <div className='HomePageContainer'>{isJoin ? "JUNTAR" : 'CRIAR'}</div>
+                    </Grid>
+                </Grid>
             </Grid>
         </>
     )
